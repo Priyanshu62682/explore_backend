1fd6edd7655efa00b4b7eaa79627107749548513
bishnu-sahu <bishnukishoresahu@gmail.com>
2016-11-23 13:12:11 +0530

    herokuuuuu

--- explore/settings.py
+++ explore/settings.py
@@ -23,11 +23,13 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't$$b1m8ccoef6t@i&ptr5xa66+=*)d%70@76jt8go7nkcvt0uk'
+#SECRET_KEY = 't$$b1m8ccoef6t@i&ptr5xa66+=*)d%70@76jt8go7nkcvt0uk'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 ALLOWED_HOSTS = []
 
 
