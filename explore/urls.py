f374057c56aa4eec47d5e13ac944869d978366e7
priyanshu <priyanshu62682@gmail.com>
2016-11-29 18:14:26 +0530

    url spaces

--- explore/urls.py
+++ explore/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     url(r'^answerpost/(?P<answer>[a-z]+)/(?P<quw>[0-9]+)/(?P<usr_id>[0-9]+)/$', views.answerPost.as_view()),
 #    url(r'^feed/(?P<id_input>[0-9]+)/$',views.questionList.as_view()),
 #    url(r'^answerlist/(?P<question_id>[0-9]+)/$',views.answerList.as_view()),
-    url(r'^questionpost/(?P<usr_id>[0-9]+)/(?P<que_detail>[a-z]+)/(?P<loc>[a-z]+)/$',views.questionPost.as_view()),
+    url(r'^questionpost/(?P<usr_id>[0-9]+)/(?P<que_detail>[a-zA-Z0-9|\w|\W]+)/(?P<loc>[a-z]+)/$',views.questionPost.as_view()),
     url(r'^questionlike/(?P<usr_id>[0-9]+)/(?P<que_id>[0-9]+)/$',views.question_vote_upvote.as_view()),
     url(r'^questiondislike/(?P<usr_id>[0-9]+)/(?P<que_id>[0-9]+)/$',views.question_vote_downvote.as_view()),
     url(r'^answerlike/(?P<usr_id>[0-9]+)/(?P<que_id>[0-9]+)/$',views.answer_vote_upvote.as_view()),
